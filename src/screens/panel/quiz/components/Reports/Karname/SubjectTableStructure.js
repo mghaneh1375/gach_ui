467fe385bd6c1ffda3dcd2d70fffc70e56cfda3a
mghaneh1375 <mghaneh1375@gmail.com>
2022-08-14 17:02:28 +0430

    percents added to answer_sheet

---
+++ src/screens/panel/quiz/components/Reports/Karname/SubjectTableStructure.js
@@ -0,0 +1,44 @@
+const columns = [
+  {
+    name: 'نام مبحث ',
+    selector: row => row.name,
+    grow: 3,
+  },
+  {
+    name: 'تعداد سوال',
+    selector: row => row.total,
+    grow: 1,
+  },
+  {
+    name: 'درست',
+    selector: row => row.corrects,
+    grow: 1,
+  },
+  {
+    name: 'نادرست',
+    selector: row => row.incorrects,
+    grow: 1,
+  },
+  {
+    name: 'نزده',
+    selector: row => row.whites,
+    grow: 1,
+  },
+  {
+    name: 'درصد',
+    selector: row => row.percent,
+    grow: 1,
+  },
+  {
+    name: 'نمره تراز',
+    selector: row => row.taraz,
+    grow: 1,
+  },
+  {
+    name: 'رتبه در استان',
+    selector: row => row.stateRank,
+    grow: 1,
+  },
+];
+
+export default columns;
