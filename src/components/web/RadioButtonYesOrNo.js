748db898dbbcabfd282a71e23e941d2cbcbd826e
alborz-moon <alborzmoon@gmail.com>
2022-08-22 22:35:16 +0430

    Optimize of RadioButton

--- src/components/web/RadioButtonYesOrNo.js
+++ src/components/web/RadioButtonYesOrNo.js
@@ -3,16 +3,14 @@ import commonTranslator from '../../tranlates/Common';
 import React from 'react';
 function RadioButtonYesOrNo(props) {
   return (
-    <PhoneView style={{marginTop: 8}}>
-      <SimpleText text={props.label} />
+    <PhoneView>
+      <SimpleText style={{alignSelf: 'center'}} text={props.label} />
       <CommonRadioButton
-        style={{marginTop: -3}}
         status={props.selected === 'yes' ? 'checked' : 'unchecked'}
         onPress={() => props.setSelected('yes')}
         text={commonTranslator.yes}
       />
       <CommonRadioButton
-        style={{marginTop: -3}}
         status={props.selected === 'no' ? 'checked' : 'unchecked'}
         onPress={() => props.setSelected('no')}
         text={commonTranslator.no}
